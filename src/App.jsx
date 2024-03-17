e4474bedf4b4021ab8c1e229537cc79f6218f131
Najas06 <najasneju@gmail.com>
2024-03-18 00:33:51 +0530

    setup-tailwind

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main className=' relative'>
+      Nav 
+      <section className=' xl:padding-1 wide:padding-r padding-b'>
+        Hero
+      </section>
+      <section className='padding'>
+        PopularProducts 
+      </section>
+    </main>
+    )
+}
+
+export default App
